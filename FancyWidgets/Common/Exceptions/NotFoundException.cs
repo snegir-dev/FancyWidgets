bae4708aadff05902119f734ab2c0b554fb6c547
SnEG1R <steamsnegir@gmail.com>
2023-07-10 21:01:56 +0300

    refactor exception handling and add TryGetValue methods

--- FancyWidgets/Common/Exceptions/NotFoundException.cs
+++ FancyWidgets/Common/Exceptions/NotFoundException.cs
@@ -2,8 +2,19 @@
 
 public class NotFoundException : Exception
 {
+    public NotFoundException()
+    {
+    }
+
     public NotFoundException(string? message)
         : base(message)
     {
     }
+
+    public static NotFoundException ThrowNotFoundException(string id) =>
+        throw new NotFoundException($"Element with id - {id} not found.");
+
+    public static NotFoundException ThrowNotFoundException(string fullNameClass, string propertyName) =>
+        throw new NotFoundException($"Element with FullClassName - " +
+                                    $"{fullNameClass} and PropertyName - {propertyName} not found.");
 }
